279dfa4f0ca7b7ac2deb91063c6e4cf2bced1393
VlaKiy <65946797+VlaKiy@users.noreply.github.com>
2021-07-16 15:43:29 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.slider_first').slick({
+        slidesToShow: 5,
+        slidesToScroll: 3
+    });
+
+    $('.slider_second').slick({
+        slidesToShow: 5,
+        slidesToScroll: 3
+    });
+
+    $('.slider_third').slick({
+        slidesToShow: 4,
+        slidesToScroll: 3
+    });
+
+    $('.slider_fourth').slick({
+        slidesToShow: 5,
+        slidesToScroll: 3
+    });
+});
+
