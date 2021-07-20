e4fbd4c7a050d74cd6bd8fca9919fa3c5356d787
VlaKiy <65946797+VlaKiy@users.noreply.github.com>
2021-07-20 15:10:32 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -198,5 +198,24 @@ $(document).ready(function(){
           
         ]
     });
+
+    $('.hero_section_header_nav a').click(function(){
+        /*задали какой мы хотим отступ от верха страницы*/
+        var otstupTop=100;
+        $('body,html').animate({
+        /*получили положение элемента вычли отступ и прокрутили*/
+            scrollTop: $($(this).attr('href')).offset().top-otstupTop
+        }, 1500);
+    });
+
+    $('.menu__box a').click(function(){
+      /*задали какой мы хотим отступ от верха страницы*/
+      var otstupTop=100;
+      $('body,html').animate({
+      /*получили положение элемента вычли отступ и прокрутили*/
+          scrollTop: $($(this).attr('href')).offset().top-otstupTop
+      }, 1500);
+  });
+  
 });
 
